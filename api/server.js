1f00f5d0719e0dbeeaae9b7b611632e8ea2c468f
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-10-14 16:12:40 -0700

    Welcome router ready

--- api/server.js
+++ api/server.js
@@ -1,9 +1,17 @@
 const express = require("express");
-
+const welcomeRouter = require('../routers/welcomeRouter');
 const db = require("../data/dbConfig.js");
 
 const server = express();
 
 server.use(express.json());
+server.use(welcomeRouter)
+
+server.use((err, req, res, next) => {
+    console.log(err)
+    return res.status(500).json({
+        errorMessage: 'Something went wrong server-side'
+    })
+})
 
 module.exports = server;
